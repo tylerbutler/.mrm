1f043b60b678d3cdb7e558532a9d3384dcbe7777
Tyler Butler <tylerbu@microsoft.com>
2022-04-18 14:51:31 -0700

    wip

--- nx-tasks/index.js
+++ nx-tasks/index.js
@@ -1,14 +1,160 @@
-// Mrm module to work with new line separated text files
-const { lines } = require('mrm-core');
+// eslint-disable-next-line unicorn/import-style
+import * as path from 'path';
+import * as process from 'process';
+import mrm from 'mrm-core';
+import { globby, globbyStream } from 'globby';
 
-module.exports = function task() {
-  // Read .gitignore if it exists
-  lines('.gitignore')
-    // Add lines that do not exist in a file yet,
-    // but keep all existing lines
-    .add(['node_modules/', '.DS_Store'])
-    // Update or create a file
-    .save();
-};
+const { json, lines, packageJson } = mrm;
 
-module.exports.description = 'Adds .gitignore';
+const FLUID_ROOT = 'C:\\code\\FluidFramework';
+const STARTING_PATH = process.cwd();
+
+(async () => {
+  for await (const p of globbyStream('**/package.json', { gitignore: true })) {
+    const absPath = path.dirname(path.resolve(STARTING_PATH, p));
+    process.chdir(absPath);
+
+    console.log(process.cwd());
+    console.log(absPath);
+    const relPath = path.relative(FLUID_ROOT, absPath).split(path.sep).join(path.posix.sep);
+    console.log(relPath);
+
+    const pkgName = json(path.join(absPath, 'package.json')).get('name');
+
+    // if (json(path.join(absPath, 'package.json')).exists()) {
+    // }
+    // packageJson()
+    const isPrivate = json(path.join(absPath, 'package.json')).get('private');
+    if (!isPrivate) {
+      let tags = [];
+      if (absPath.includes('common')) {
+        tags = ['scope:common'];
+      } else if (absPath.includes('examples')) {
+        tags = ['scope:examples'];
+      } else if (absPath.includes('server')) {
+        tags = ['scope:server'];
+      } else {
+        tags = ['scope:client'];
+      }
+
+      json(path.join(absPath, 'package.json'))
+        .set('nx.tags', tags)
+        .save();
+
+      // json(path.join(absPath, 'package.json'))
+      //   .set('scripts.build', `nx build:compile ${pkgName} && nx lint ${pkgName} && nx build:docs ${pkgName}`)
+      //   .set('scripts.build:commonjs', `nx tsc ${pkgName} && nx build:test ${pkgName}`)
+      //   .set('scripts.build:compile', `nx build:commonjs ${pkgName} && nx build:esnext ${pkgName}`)
+      //   .set('scripts.build:full', `nx build ${pkgName}`)
+      //   .set('scripts.build:full:compile', `nx build:compile ${pkgName}`)
+      //   .set('scripts.lint', `nx eslint ${pkgName}`)
+      //   .set('scripts.lint:fix', `nx eslint:fix ${pkgName}`)
+      //   .save();
+
+      json(path.join(absPath, 'project.json'))
+        .set({
+          name: pkgName,
+          root: relPath,
+          sourceRoot: path.posix.join(relPath, 'src'),
+          projectType: 'library',
+          generators: {},
+          targets: {
+            // build: {
+            //   executor: '@nrwl/workspace:run-commands',
+            //   options: {
+            //     outputPath: 'dist/',
+            //     commands: [
+            //       `nx build:compile ${pkgName}`,
+            //       `nx lint ${pkgName}`,
+            //     ],
+            //     parallel: true,
+            //     dependsOn: [
+            //       {
+            //         target: 'build:compile',
+            //         projects: 'dependencies',
+            //       },
+            //     ],
+            //   },
+            // },
+            // 'build:compile': {
+            //   executor: '@nrwl/workspace:run-commands',
+            //   options: {
+            //     outputPath: ['dist'],
+            //     commands: [
+            //       `nx build:commonjs ${pkgName}`,
+            //       `nx build:esnext ${pkgName}`,
+            //     ],
+            //     parallel: true,
+            //     dependsOn: [
+            //       {
+            //         target: 'tsc',
+            //         projects: 'dependencies',
+            //       },
+            //       {
+            //         target: 'build:test',
+            //         projects: 'dependencies',
+            //       },
+            //       {
+            //         target: 'build:esnext',
+            //         projects: 'dependencies',
+            //       },
+            //     ],
+            //   },
+            // },
+            // 'build:commonjs': {
+            //   executor: '@nrwl/workspace:run-commands',
+            //   options: {
+            //     outputPath: ['dist'],
+            //     commands: [
+            //       `nx tsc ${pkgName}`,
+            //       `nx build:test ${pkgName}`,
+            //     ],
+            //     parallel: false,
+            //     dependsOn: [
+            //       {
+            //         target: 'tsc',
+            //         projects: 'dependencies',
+            //       },
+            //       {
+            //         target: 'build:test',
+            //         projects: 'dependencies',
+            //       },
+            //     ],
+            //   },
+            // },
+            'build:esnext': {
+              executor: '@nrwl/workspace:run-script',
+              outputPath: ['libs'],
+              options: {
+                script: 'build:esnext',
+              },
+            },
+            // lint: {
+            //   executor: '@nrwl/workspace:run-commands',
+            //   options: {
+            //     outputPath: ['dist'],
+            //     commands: [
+            //       `nx eslint ${pkgName}`,
+            //     ],
+            //     parallel: true,
+            //     dependsOn: [],
+            //   },
+            // },
+          },
+        })
+        .save();
+    }
+    //   .removeScript('build')
+    //   .removeScript('build:commonjs')
+    //   .removeScript('build:compile')
+    //   .removeScript('build:full')
+    //   .removeScript('build:full:compile')
+    //   .removeScript('lint')
+    //   .removeScript('lint:fix')
+    //   .save();
+
+    // process.chdir(STARTING_PATH);
+  }
+
+  console.log('Done');
+})();
